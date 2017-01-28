c377cd571bbfe191e2afd78fe72978054a447f32
mbasmanov92 <mbasmanon92@gmail.com>
2017-01-28 19:15:43 +0700

    готовое задание

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,14 @@
+'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  'use strict';
-  var width, height, indentX, indentY, name, i, timesInt, maxTime, time;
+  var width;
+  var height;
+  var indentX;
+  var indentY;
+  var name;
+  var i;
+  var maxTime;
+  var time;
+  var randomBlue;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -14,32 +22,30 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'blue';
   ctx.fillText('Список результатов:', 200, 65);
 
-  timesInt = [];
-  for (i = 0; i < times.length; i = i + 1) {
-    timesInt[i] = Math.round(times[i] / 1000);
-  }
-
   maxTime = -1;
-  for (i = 0; i < timesInt.length; i = i + 1) {
-    time = timesInt[i];
+  for (i = 0; i < times.length; i = i + 1) {
+    time = times[i];
     if (time > maxTime) {
       maxTime = time;
     }
   }
 
   indentX = 160;
-  for (i = 0; i < timesInt.length; i = i + 1) {
+  for (i = 0; i < times.length; i = i + 1) {
     name = names[i];
-    time = timesInt[i];
+    time = Math.round(times[i]);
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(250, 0, 0, 1)';
     } else {
-      ctx.fillStyle = ['rgba(0, 0, ', ((Math.random() * 5) * 50).toFixed(0), ',', (Math.random()).toFixed(1), ')'].join;
+      randomBlue = ((Math.random() * 5) * 50).toFixed(0);
+      ctx.fillStyle = 'rgba(0, 0,' + randomBlue + ', 1)';
     }
     width = 40;
     height = 150 * time / maxTime;
-    indentY = 230 - (150 * time / maxTime);
+    indentY = 250 - (150 * time / maxTime);
     ctx.fillRect(indentX, indentY, width, height);
+    ctx.fillText(time, indentX, indentY - 10);
+    ctx.fillText(name, indentX, 270);
     indentX = indentX + 90;
   }
 };
